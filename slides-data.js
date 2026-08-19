a70dfa4a223c50da8ba80dd8e890e5cbdb29b4db
Jormungand-S <carlos.roberto.number9@gmail.com>
2026-08-19 10:57:52 -0300

    style: use light logo with rounded borders

--- slides-data.js
+++ slides-data.js
@@ -12,7 +12,7 @@ const slidesData = [
         ],
         rightContent: `
             <div class="orbit-diagram">
-                <div class="center-node"><img src="logo-dark.png" style="width: 80%; height: 80%; object-fit: contain;"></div>
+                <div class="center-node"><img src="logo-light.png" style="width: 100%; height: 100%; object-fit: cover; border-radius: inherit;"></div>
                 <div class="orbit-item item-1">
                     <h4>COMUNICAÇÃO</h4>
                     <p>Autoridade + confiança</p>
